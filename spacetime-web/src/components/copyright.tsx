09550d42189a717fab37c86001ce7b9dfdb6d98e
Eduardo Sartori <115821181+oeduardosartori@users.noreply.github.com>
2023-05-22 14:52:52 -0300

    Add files via upload

---
+++ spacetime-web/src/components/copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da Rocketseat por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://github.com/oeduardosartori/"
+      >
+        Eduardo Sartori
+      </a>
+    </div>
+  )
+}
